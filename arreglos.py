091bddf536154682cf03ebdde25badbb23c4a15d
yountek14 <benjita1b4@gmail.com>
2025-04-21 13:29:58 -0400

    Segundo Commit, avances de la clase de arreglos

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,15 @@
 #hola soy un comentario :3
+
+frutas = ["manzana", "banana", "naranja", "kiwi", "mango"]
+print(frutas[2]) 
+
+estudiantes = [
+    {"nombre" : "Maxi" , "edad" : 20, "curso" : "Segundo A"},
+    {"nombre" : "Benja" , "edad" : 21, "curso" : "Primero B"},
+]
+print(estudiantes[0]["nombre"])
+print(estudiantes[0]["edad"])
+print(estudiantes[0]["curso"])
+print(estudiantes[1]["nombre"])
+print(estudiantes[1]["edad"])
+print(estudiantes[1]["curso"])   
